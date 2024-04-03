989c7a4291d463c9218f9b50200af9cfa952c1e8
Chris309283 <Christopher.holmelund@gmail.com>
2024-04-03 17:41:02 +0200

    DispatcherInteractionTest.cs made

---
+++ Tests/UnitTests/Common/Dispatcher/CreateEventMockHandler.cs
@@ -0,0 +1,16 @@
+﻿using ViaEventAssociation.Core.AppEntry.Commands;
+using ViaEventAssociation.Core.AppEntry.Commands.Event;
+using ViaEventAssociation.Core.Tools.OperationResult.OperationResult;
+
+namespace UnitTests.Common.Dispatcher;
+
+public class CreateEventMockHandler : ICommandHandler<ViaEventCreateCommand>
+{
+    public Task<OperationResult> HandleAsync(ViaEventCreateCommand command)
+    {
+        WasCalled = true;
+        return Task.FromResult(OperationResult.Success());
+    }
+
+    public bool WasCalled { get; set; }
+}
---
+++ Tests/UnitTests/Common/Dispatcher/DispatcherInteractionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.AppEntry.Commands;
+using ViaEventAssociation.Core.AppEntry.Commands.Event;
+using ViaEventAssociation.Core.AppEntry.Dispatcher;
+
+namespace UnitTests.Common.Dispatcher;
+
+public class DispatcherInteractionTest
+{
+    [Fact]
+    public async Task DispatchAsync_SingleHandlerRegistered_Success()
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddScoped<ICommandHandler<ViaEventCreateCommand>, CreateEventMockHandler>();
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        
+        ICommandDispatcher dispatcher = new CommandDispatcher(serviceProvider);
+        var cmd = ViaEventCreateCommand.Create().Payload;
+        
+        var result = await dispatcher.DispatchAsync(cmd);
+        
+        Assert.True(result.IsSuccess);
+        var handler = (CreateEventMockHandler)serviceProvider.GetService<ICommandHandler<ViaEventCreateCommand>>()!;
+        Assert.True(handler.WasCalled);
+    }
+}
