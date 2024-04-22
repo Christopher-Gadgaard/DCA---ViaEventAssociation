96cca295fd91ee8900707df1d0ffb59711fcdcea
Vlad Lazar <lazar.vlad151@gmail.com>
2024-04-22 20:47:01 +0200

    scaffolding

---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ViaEventAssociation.Infrastructure.EfcQueries;
+
+public partial class Event
+{
+    public string Id { get; set; } = null!;
+
+    public string EventTitle { get; set; } = null!;
+
+    public string EventDescription { get; set; } = null!;
+
+    public string? StartDate { get; set; }
+
+    public string? EndDate { get; set; }
+
+    public int MaxGuests { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public string Visibility { get; set; } = null!;
+
+    public virtual ICollection<ViaGuestId> ViaGuestIds { get; set; } = new List<ViaGuestId>();
+
+    public virtual ICollection<ViaInvitation> ViaInvitations { get; set; } = new List<ViaInvitation>();
+}
---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/ViaGuestId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ViaEventAssociation.Infrastructure.EfcQueries;
+
+public partial class ViaGuestId
+{
+    public int Id { get; set; }
+
+    public string ViaEventId { get; set; } = null!;
+
+    public virtual Event ViaEvent { get; set; } = null!;
+}
