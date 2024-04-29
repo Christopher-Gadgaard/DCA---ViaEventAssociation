c734e4b746d0d908a59bf70c5acda954bd13847c
Vlad Lazar <lazar.vlad151@gmail.com>
2024-04-29 17:24:19 +0200

    view event details

---
+++ src/Core/ViaEventAssociation.Core.QueryContracts/Queries/ViewEvent.cs
@@ -0,0 +1,25 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Contracts;
+
+namespace ViaEventAssociation.Core.QueryContracts.Queries;
+
+public class ViewEvent
+{
+    public record Query(string EventId, int PageNumber, int DisplayedRows, int RowSize) : IQuery<Answer>;
+    public record Answer(Event Event, List<Guest> Guest, int GuestsCount);
+
+    public record Event(
+        string Title,
+        string Description,
+        string From,
+        string To,
+        string Visibility,
+        int MaxGuests
+    );
+    
+    public record Guest(
+        string Id,
+        string FirstName,
+        string LastName,
+        string PictureUrl
+    );
+}
---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/QueryHandlers/ViewEventQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.QueryContracts.Contracts;
+using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Infrastructure.EfcQueries.DTOs;
+
+namespace ViaEventAssociation.Infrastructure.EfcQueries.QueryHandlers;
+
+public class ViewEventQueryHandler
+    (VeadatabaseProductionContext context) : IQueryHandler<ViewEvent.Query, ViewEvent.Answer>
+{
+    public async Task<ViewEvent.Answer> HandleAsync(ViewEvent.Query query)
+    {
+        var singleEvent = await context.Events.Where(e => e.Id == query.EventId).Select(e =>
+                new ViewEvent.Event(e.EventTitle, e.EventDescription, e.StartDate, e.EndDate, e.Visibility,
+                    e.MaxGuests))
+            .FirstOrDefaultAsync();
+        
+        var inviteGuestIds = await context.ViaInvitations
+            .Where(i => i.ViaEventId == query.EventId && i.Status == "Accepted")
+            .Select(i => i.ViaGuestId)
+            .ToListAsync();
+        //invited Guests
+        var invitedGuests = context.Guests
+            .Where(g => inviteGuestIds.Contains(g.Id));
+        
+    return new ViewEvent.Answer(singleEvent!, new List<ViewEvent.Guest>(), invitedGuests.Count());
+
+    }
+}
