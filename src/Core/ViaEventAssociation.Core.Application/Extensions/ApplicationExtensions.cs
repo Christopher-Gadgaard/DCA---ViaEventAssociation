edc58c696eefeebde889e73eba8a6bfeda12d874
Vlad Lazar <lazar.vlad151@gmail.com>
2024-04-29 23:30:11 +0200

    setup

---
+++ src/Core/ViaEventAssociation.Core.Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.AppEntry.Commands;
+using ViaEventAssociation.Core.AppEntry.Commands.Event;
+using ViaEventAssociation.Core.Application.Features.Event;
+
+namespace ViaEventAssociation.Core.Application.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void RegisterHandlers(this IServiceCollection services)
+    {
+        services.AddScoped<ICommandHandler<ViaEventCreateCommand>, ViaEventCreateHandler>();
+    }
+}
---
+++ src/Core/ViaEventAssociation.Core.Application/Extensions/DispatcherExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.AppEntry.Dispatcher;
+
+namespace ViaEventAssociation.Core.Application.Extensions;
+
+public static class DispatcherExtensions
+{
+    
+    public static void RegisterDispatcher(this IServiceCollection services)
+    {
+        services.AddScoped<ICommandDispatcher>();
+    }
+}
