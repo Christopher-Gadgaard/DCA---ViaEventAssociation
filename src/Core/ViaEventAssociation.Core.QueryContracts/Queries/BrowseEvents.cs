1962b2bb89ad4592311a703520562e021ae70073
Vlad Lazar <lazar.vlad151@gmail.com>
2024-04-29 17:08:15 +0200

    browse events

---
+++ src/Core/ViaEventAssociation.Core.QueryContracts/Queries/BrowseEvents.cs
@@ -0,0 +1,18 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Contracts;
+
+namespace ViaEventAssociation.Core.QueryContracts.Queries;
+
+public class BrowseEvents
+{
+    public record Query(int PageNumber, int PageSize,string SearchedText) : IQuery<Answer>;
+
+    public record Answer(List<Event> Events, int MaxPageNum);
+
+    public record Event(
+        string Id,
+        string Title,
+        string Description,
+        int MaxGuests,
+        int GuestCount,
+        string From);
+}
---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/QueryHandlers/BrowseEventsQueryHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.QueryContracts.Contracts;
+using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Infrastructure.EfcQueries.DTOs;
+
+namespace ViaEventAssociation.Infrastructure.EfcQueries.QueryHandlers;
+
+public class BrowseEventsQueryHandler
+    (VeadatabaseProductionContext context) : IQueryHandler<BrowseEvents.Query, BrowseEvents.Answer>
+{
+    public async Task<BrowseEvents.Answer> HandleAsync(BrowseEvents.Query query)
+    {
+        var time = DateTime.Now;
+        var allUpcomingEvents = context.Events
+            .Where(e => 
+                e.StartDate.CompareTo(time) > 0 
+                && e.EventTitle.Contains(query.SearchedText)
+            );
+
+        var upcomingEvents = allUpcomingEvents
+            .OrderByDescending(e => e.StartDate);
+        
+        //get the total number of invitations for each of these events
+        var numberOfInvitations = await context.ViaInvitations
+            .Where(i => upcomingEvents.Select(e => e.Id).Contains(i.ViaEventId))
+            .GroupBy(i => i.ViaEventId)
+            .Select(g => new {EventId = g.Key, Count = g.Count()})
+            .ToDictionaryAsync(g => g.EventId, g => g.Count);
+        
+       
+        
+        //combine the events with the number of invitations and requests
+        var upcomingEventsWithParticipants = await upcomingEvents.Select(e => new BrowseEvents.Event(
+                e.Id,
+                e.EventTitle,
+                e.EventDescription,
+                e.MaxGuests,
+                numberOfInvitations.GetValueOrDefault(e.Id, 0),
+                e.StartDate
+            )).ToListAsync();
+        var maxPageNum = allUpcomingEvents.Count() / query.PageSize;
+        if (maxPageNum % query.PageSize > 0)
+        {
+            maxPageNum++;
+        }
+
+  
+        return new BrowseEvents.Answer(upcomingEventsWithParticipants, maxPageNum);
+    }
+}
