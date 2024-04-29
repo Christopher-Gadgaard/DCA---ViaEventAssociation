3eb35c1da85fc2350cdd1d152d248d534a1cf909
Vlad Lazar <lazar.vlad151@gmail.com>
2024-04-29 17:41:33 +0200

    unpublished events

---
+++ src/Core/ViaEventAssociation.Core.QueryContracts/Queries/UnpublishedEvents.cs
@@ -0,0 +1,20 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Contracts;
+
+namespace ViaEventAssociation.Core.QueryContracts.Queries;
+
+public class UnpublishedEvents
+{
+    public record Query() : IQuery<Answer>;
+
+    public record Answer(
+        List<Event> DraftEvents,
+        List<Event> ReadyEvents,
+        List<Event> CancelledEvents
+    );
+        
+    public record Event(
+        string Id,
+        string Title,
+        string Status
+    );
+}
---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/QueryHandlers/UnpublishedEventsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.QueryContracts.Contracts;
+using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Infrastructure.EfcQueries.DTOs;
+
+namespace ViaEventAssociation.Infrastructure.EfcQueries.QueryHandlers;
+
+public class UnpublishedEventsQueryHandler(VeadatabaseProductionContext context): IQueryHandler<UnpublishedEvents.Query, UnpublishedEvents.Answer>
+{
+    public async Task<UnpublishedEvents.Answer> HandleAsync(UnpublishedEvents.Query query)
+    {
+        var draftEvents = await context.Events
+            .Where(e => e.Status == "Draft")
+            .Select(e => new UnpublishedEvents.Event(e.Id, e.EventTitle, e.Status))
+            .ToListAsync();
+        
+        var readyEvents = await context.Events
+            .Where(e => e.Status == "Ready")
+            .Select(e => new UnpublishedEvents.Event(e.Id, e.EventTitle, e.Status))
+            .ToListAsync();
+        
+        var cancelledEvents = await context.Events
+            .Where(e => e.Status == "Cancelled")
+            .Select(e => new UnpublishedEvents.Event(e.Id, e.EventTitle, e.Status))
+            .ToListAsync();
+        
+        return new UnpublishedEvents.Answer(draftEvents, readyEvents, cancelledEvents);
+    }
+}
